f6c75500f2e2f8ff5bb6109a6d193134e6e2441c
Julia Tufts <juliatufts@gmail.com>
2021-02-04 00:08:26 -0400

    Bind svg trombone slider left offset to input slider value

--- js/range-fill.js
+++ js/range-fill.js
@@ -1,9 +1,16 @@
 
 (() => {
     const pitchControl = document.querySelector('#pitch');
+    const pitchMin = pitchControl.getAttribute("min");
+    const pitchMax = pitchControl.getAttribute("max");
+    const sliderMax = 140;
+    const svgSlider = document.querySelector('svg.slider');
+
     window.addEventListener('load', () => {
         pitchControl.addEventListener('input', e => {
             pitchControl.style.setProperty('--val', pitchControl.value);
+            let pitchNormalized = (pitchControl.value - pitchMin) / (pitchMax - pitchMin);
+            svgSlider.style.setProperty('--left', (pitchNormalized * sliderMax) + "px");
         }, false);
     });
 })();
